df75bd5ecf4b7795a1f07696a9b8d81d056742e7
Rafael Amãncio <52306753+rafaelThe50@users.noreply.github.com>
2021-09-03 20:23:31 -0300

    tabela familia

---
+++ packages/server/src/app/entities/Family.ts
@@ -0,0 +1,90 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity('family')
+class Family {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  nameResp1: string
+
+  @Column()
+  cpfResp1: string
+
+  @Column()
+  nameMotherResp1: string
+
+  @Column()
+  dateBirthResp1: Date
+
+  @Column()
+  sexResp1: string
+
+  @Column()
+  fetchBasketResp1: boolean
+
+  @Column()
+  nis1: string
+
+  @Column()
+  nameResp2: string
+
+  @Column()
+  cpfResp2: string
+
+  @Column()
+  nameMotherResp2: string
+
+  @Column()
+  dateBirthResp2: Date
+
+  @Column()
+  sexResp2: string
+
+  @Column()
+  fetchBasketResp2: boolean
+
+  @Column()
+  nis2: string
+
+  @Column()
+  address: string
+
+  @Column()
+  numDep: string
+
+  @Column()
+  phone: string
+
+  @Column()
+  incomeCapita: string
+
+  @Column()
+  entityServide: string
+
+  @Column()
+  descAgeRange: string
+
+  @Column()
+  totalResiden: number
+
+  @Column()
+  socialService: string
+
+  @Column()
+  numAbsence: string
+
+  @Column()
+  basket: string
+
+  @Column()
+  dataInit: Date
+
+  @Column()
+  dateEnd: Date
+
+  @Column()
+  typeReceive: string
+}
+
+export default Family
--- packages/server/src/database/migrations/1630630149702-User.ts
+++ packages/server/src/database/migrations/1630630149702-User.ts
@@ -6,7 +6,7 @@ export class User1630630149702 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
     }
-
+   
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "users"`);
     }
