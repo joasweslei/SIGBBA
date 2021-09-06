a263f8da9a94721579b3328f4f7b7be296eeba2e
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-09-06 19:03:12 -0300

    feat: adiciona theme com emotion

---
+++ packages/web/src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { green, teal } from '@material-ui/core/colors'
+import { createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: green
+  }
+})
+
+export default theme
