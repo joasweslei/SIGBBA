1cda92b5298f108fca8d34f288d7852f8b9d7567
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-09-06 19:04:07 -0300

    feat: adiciona componentes para estruturacao padrao de paginas no projeto

---
+++ packages/web/src/app/components/DefaultSchemas/AppBarSchema.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import MoreIcon from '@material-ui/icons/MoreVert'
+import {
+  AppBar,
+  Box,
+  IconButton,
+  styled,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  alignItems: 'flex-start',
+  paddingTop: theme.spacing(1),
+  paddingBottom: theme.spacing(2),
+  '@media all': {
+    minHeight: 128
+  }
+}))
+
+export const AppBarSchema = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <StyledToolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, alignSelf: 'flex-end' }}
+          >
+            Sistema Gerenciador do Banco de Alimentos
+          </Typography>
+          <IconButton size="large" aria-label="search" color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="display more actions"
+            edge="end"
+            color="inherit"
+          >
+            <MoreIcon />
+          </IconButton>
+        </StyledToolbar>
+      </AppBar>
+    </Box>
+  )
+}
