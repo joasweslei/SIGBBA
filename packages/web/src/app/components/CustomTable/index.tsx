bba04007e4734c46337aebfe4d973636f3f83cc5
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-09-07 17:11:09 -0300

    fix: alinhamento da paginacao da CustomTable

--- packages/web/src/app/components/CustomTable/index.tsx
+++ packages/web/src/app/components/CustomTable/index.tsx
@@ -45,7 +45,6 @@ export const CustomTable: React.FC<CustomTableProps> = ({
           <TableRow>
             <TablePagination
               rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
               count={itemCount}
               rowsPerPage={rowsPerPage}
               page={currentPage}
