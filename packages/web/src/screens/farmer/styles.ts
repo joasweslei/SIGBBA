89a5e3519474f7b347727b61f5e4090a18b8dced
Rafael Amãncio <52306753+rafaelThe50@users.noreply.github.com>
2021-09-07 17:00:26 -0300

    tela agricultor

---
+++ packages/web/src/screens/farmer/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from '@material-ui/core'
+import { StyledTableCell } from '../../app/components/StyledTableCell'
+
+export const StyledTableCellCustom = styled(StyledTableCell)(({ theme }) => ({
+  textAlign: 'center'
+}))
--- packages/web/src/screens/routes.tsx
+++ packages/web/src/screens/routes.tsx
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './home'
 import { AlimentBasket } from './AlimentBasket'
 import DonationOrder from './donationOrder'
+import Farmer from './farmer'
+
 
 const Routes: React.FC = () => {
   return (
@@ -11,6 +13,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={Home} />
       <Route path="/aliment-basket" exact component={AlimentBasket} />
       <Route path="/Doacoes" exact component={DonationOrder} />
+      <Route path="/Farmer" exact component={Farmer} />
     </Switch>
   )
 }
