d14278af6d67f7e831857321ed88ce3f079d3cc1
ThiagoMB60 <thiagomoreira60@gmail.com>
2021-09-07 15:07:26 -0300

    feat: modelo inicial de lista de pedidos de doação

--- packages/web/src/screens/routes.tsx
+++ packages/web/src/screens/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './home'
+import DonationOrder from './donationOrder'
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/Doacoes" exact component={DonationOrder} />
     </Switch>
   )
 }
