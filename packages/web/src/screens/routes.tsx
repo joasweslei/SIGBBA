181b608ad66783c598fcb372acdac32962a9f7e5
VitoriaSant <svitoria3465@gmail.com>
2021-09-09 00:51:24 -0300

    refactor

---
+++ packages/web/src/screens/routes.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './home'
+import { AlimentBasketList } from './AlimentBasket/List'
+import { AlimentBasketForm } from './AlimentBasket/Form'
+import OrderList from './orderList'
+import FarmerList from './farmer/List'
+import FarmerForm from './farmer/Form'
+import { Login } from './auth/Login'
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/aliment-basket" exact component={AlimentBasketList} />
+      <Route path="/aliment-basket/form" exact component={AlimentBasketForm} />
+
+      <Route path="/pedidos" exact component={OrderList} />
+      <Route path="/farmer" exact component={FarmerList} />
+      <Route path="/farmer/form" exact component={FarmerForm} />
+      <Route path="/login" exact component={Login} />
+    </Switch>
+  )
+}
+
+export default Routes
