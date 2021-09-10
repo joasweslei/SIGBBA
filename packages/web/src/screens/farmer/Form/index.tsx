22c476b9adcd65ec07692d9ccfc9970a12b670fb
Rafael Amãncio <52306753+rafaelThe50@users.noreply.github.com>
2021-09-09 22:34:55 -0300

    Form Agricultor (Sem dropDown)

--- packages/web/src/screens/farmer/Form/index.tsx
+++ packages/web/src/screens/farmer/Form/index.tsx
@@ -13,9 +13,63 @@ const FarmerForm = () => {
          }}
         noValidate
         autoComplete="off">
-          <TextField sx={{width: '43ch'}} id="outlined-basic" label="Nome Agricultor" variant="outlined" />
-          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Nome Mãe" variant="outlined" />
-          
+          <TextField sx={{width: '44ch'}} id="outlined-basic" label="Nome Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="CPF Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Sexo" variant="outlined" />
+          <TextField sx={{width: '44ch'}} id="outlined-basic" label="Nome da Mãe do Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Data Nascimento Responsável" variant="outlined" />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1},
+          paddingTop: '2%'
+         }}
+        noValidate
+        autoComplete="off">
+          <TextField sx={{width: '44ch'}} id="outlined-basic" label="Nome Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="CPF Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Sexo" variant="outlined" />
+          <TextField sx={{width: '44ch'}} id="outlined-basic" label="Nome da Mãe do Responsável" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Data Nascimento Responsável" variant="outlined" />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1},
+          paddingTop: '2%'
+         }}
+        noValidate
+        autoComplete="off">
+          <TextField sx={{width: '64ch'}} id="outlined-basic" label="Endereço Completo" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Município" variant="outlined" />
+          <TextField sx={{width: '10ch'}} id="outlined-basic" label="UF" variant="outlined" />
+          <TextField sx={{width: '44ch'}} id="outlined-basic" label="Telefone" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="NIS" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Validade do Alvará" variant="outlined" />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1},
+          paddingTop: '2%'
+         }}
+        noValidate
+        autoComplete="off">
+          <TextField sx={{width: '50ch'}} id="outlined-basic" label="DAPI" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Validade DAPI" variant="outlined" />
+          <TextField sx={{width: '24ch'}} id="outlined-basic" label="Alimento à ser comprado" variant="outlined" />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1},
+          paddingTop: '2%'
+         }}
+        noValidate
+        autoComplete="off">
+          <TextField sx={{width: '50ch'}} id="outlined-basic" label="Cartão produtor" variant="outlined" />
+          <TextField sx={{width: '30ch'}} id="outlined-basic" label="Senha Cartão Produtor" type="password" variant="outlined" />
       </Box>
     </FormContainer>
   )
