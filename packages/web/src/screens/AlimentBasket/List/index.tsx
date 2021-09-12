d95c8f2832b8d6fdb79f36c7f0255b8cef139372
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-09-11 23:40:09 -0300

    feat: adiciona listagem de cesta de alimentos buscando dados do backend

--- packages/web/src/screens/AlimentBasket/List/index.tsx
+++ packages/web/src/screens/AlimentBasket/List/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DefaultContainer from '../../../app/components/DefaultSchemas/Container/DefaultContainerSchema'
 import { DataGrid, GridColumns, GridRowsProp } from '@mui/x-data-grid'
+import api from '../../../config/api'
 
 export const AlimentBasketList = () => {
+  const [rows, setRows] = useState<GridRowsProp>([])
+
   const columns: GridColumns = [
     { field: 'id', headerName: 'ID', type: 'integer', width: 90 },
     { field: 'name', headerName: 'Nome', type: 'string', width: 130 },
@@ -14,23 +17,16 @@ export const AlimentBasketList = () => {
     }
   ]
 
-  const rows: GridRowsProp = [
-    {
-      id: 1,
-      name: 'Cesta Básica',
-      description: 'Uma descrição descritiva'
-    },
-    {
-      id: 2,
-      name: 'Cesta Teste',
-      description: 'Uma descrição descritiva'
-    },
-    {
-      id: 3,
-      name: 'Cesta Alo',
-      description: 'Uma descrição descritiva'
-    }
-  ]
+  useEffect(() => {
+    ;(async () => {
+      const response = await api.get('/aliment-basket')
+      if (response.status === 200) {
+        const { data } = response
+        console.log(data)
+        setRows(data)
+      }
+    })()
+  }, [])
 
   return (
     <DefaultContainer breadcrumbs={['Cesta de alimentos']}>
