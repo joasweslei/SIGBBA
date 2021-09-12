7bd2fd6d8016f2082a10993cd77f2efd2a73b875
VitoriaSant <svitoria3465@gmail.com>
2021-09-12 13:40:02 -0300

    feat: crição da tabela ProposalList da tela home

--- packages/web/src/screens/home/components/proposalList/index.tsx
+++ packages/web/src/screens/home/components/proposalList/index.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
 
-export const proposalList = () => {
-  return <p></p>
+export const ProposalList = () => {
+  const columns = [
+    { field: 'id', hide: true },
+    { field: 'proposalName', heardName: 'Proposta', width: 200 },
+    { field: 'availableValue', headerName: 'Valor Disponível', width: 170 }
+  ]
+  const rows = [
+    { id: '1', proposalName: 'CONAB', availableValue: '25000' },
+    { id: '2', proposalName: 'Ministerio', availableValue: '50000' }
+  ]
+
+  return (
+    <>
+      <div style={{ padding: 10, fontWeight: 'bold' }}>Propostas</div>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        autoHeight
+        disableColumnMenu={true}
+        pageSize={3}
+      />
+    </>
+  )
 }
