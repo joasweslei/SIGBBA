da08f98ba5cddcd93d00d45c8cfaa4a499a51a53
Rafael Amãncio <52306753+rafaelThe50@users.noreply.github.com>
2021-09-12 19:45:32 -0300

    Update routes.tsx

--- packages/web/src/screens/routes.tsx
+++ packages/web/src/screens/routes.tsx
@@ -23,15 +23,16 @@ const Routes: React.FC = () => {
       <Route path="/aliment-basket/form" exact component={AlimentBasketForm} />
 
       <Route path="/pedidos" exact component={OrderList} />
+      
       <Route path="/farmer" exact component={FarmerList} />
       <Route path="/farmer/form" exact component={FarmerForm} />
+      <Route path="/teste" exact component={DapValidation} />
 
       <Route path="/login" exact component={Login} />
       <Route path="/user" exact component={User} />
       <Route path="/user/form" exact component={UserForm} />
 
       <Route path="/family" exact component={FamilyList} />
-      <Route path="/teste" exact component={DapValidation} />
       <Route path="/family/form" exact component={FormFamily} />
     </Switch>
   )
