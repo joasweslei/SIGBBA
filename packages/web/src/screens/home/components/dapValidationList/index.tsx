d3f4d1fd817d6997cb8aa18305edc8b876d81d2f
VitoriaSant <svitoria3465@gmail.com>
2021-09-12 11:18:08 -0300

    feat: inicio da criação da tabela DapValidation na tela home

--- packages/web/src/screens/home/components/dapValidationList/index.tsx
+++ packages/web/src/screens/home/components/dapValidationList/index.tsx
@@ -1,9 +1,42 @@
+import { DataGrid } from '@mui/x-data-grid'
 import React from 'react'
 
 export const DapValidation = () => {
+  const columns = [
+    { field: 'id', hide: true, headerName: 'id' },
+    {
+      field: 'farmer',
+      headerName: 'Agricultor',
+      width: 250
+    },
+    {
+      field: 'dapDate',
+      headerName: 'Data',
+      type: 'date',
+      editable: false
+    }
+  ]
+  const rows = [
+    { id: '1', farmer: 'Agricultor A', dapDate: '2021/09/07' },
+    { id: '2', farmer: 'Agricultor B', dapDate: '2021/09/07' },
+    { id: '3', farmer: 'Agricultor C', dapDate: '2021/09/25' },
+    { id: '4', farmer: 'Agricultor D', dapDate: '2021/09/07' },
+    { id: '5', farmer: 'Agricultor 3', dapDate: '2021/09/06' },
+    { id: '6', farmer: 'Agricultor F', dapDate: '2021/09/07' },
+    { id: '7', farmer: 'Agricultor G', dapDate: '2021/09/14' },
+    { id: '8', farmer: 'Agricultor H', dapDate: '2021/09/07' },
+    { id: '9', farmer: 'Agricultor I', dapDate: '2021/09/19' },
+    { id: '10', farmer: 'Agricultor J', dapDate: '2021/09/17' }
+  ]
   return (
-    <p>
-      <p></p>
-    </p>
+    <>
+      <div style={{ padding: 10, fontWeight: 'bold' }}>Vencimentos da Dap</div>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        autoHeight
+        disableColumnMenu={true}
+      />
+    </>
   )
 }
--- packages/web/src/screens/home/index.tsx
+++ packages/web/src/screens/home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import DefaultContainer from '../../app/components/DefaultSchemas/Container/DefaultContainerSchema'
 import Grid from '@material-ui/core/Grid'
+import { Paper } from '@material-ui/core'
+import { DapValidation } from './components/dapValidationList'
 
 export const Home = () => {
   return (
@@ -10,7 +12,9 @@ export const Home = () => {
           <div style={{ backgroundColor: 'blue' }}>P</div>
         </Grid>
         <Grid item xs={4}>
-          <div style={{ backgroundColor: 'blue' }}>P</div>
+          <Paper elevation={3}>
+            <DapValidation></DapValidation>
+          </Paper>
         </Grid>
         <Grid item xs={8}>
           <div style={{ backgroundColor: 'blue' }}>P</div>
