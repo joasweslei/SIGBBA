4a151bfc36507b69656b1f156882a61fb125afc5
Rafael Amãncio <52306753+rafaelThe50@users.noreply.github.com>
2021-09-12 23:46:12 -0300

    Agricultor inicio do CRUD

--- packages/web/src/screens/AlimentBasket/List/index.tsx
+++ packages/web/src/screens/AlimentBasket/List/index.tsx
@@ -7,9 +7,9 @@ import {
   GridRowsProp,
   GridSelectionModel
 } from '@mui/x-data-grid'
-import Swal from 'sweetalert2'
 import api from '../../../config/api'
 import { useHistory } from 'react-router-dom'
+import Swal from 'sweetalert2'
 
 export const AlimentBasketList = () => {
   const history = useHistory()
--- packages/web/src/screens/farmer/List/index.tsx
+++ packages/web/src/screens/farmer/List/index.tsx
@@ -1,52 +1,119 @@
-import React from 'react'
-import { CustomTable } from '../../../app/components/CustomTable'
-
-import CreateIcon from '@material-ui/icons/Create';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-
+import React, { useEffect, useState } from 'react'
 import DefaultContainer from '../../../app/components/DefaultSchemas/Container/DefaultContainerSchema'
-import { StyledTableRow } from '../../../app/components/StyledTableRow'
-import { IconButton } from '@material-ui/core';
-import { StyledTableCellCustom } from '../../../app/components/StyledTableCellCustom';
+import { DataGrid, GridColumns, GridRowId, GridRowsProp, GridSelectionModel } from '@mui/x-data-grid';
+import Swal from 'sweetalert2';
+import { useHistory } from 'react-router';
+import api from '../../../config/api';
 
 const FarmerList: React.FC = () => {
-  const arrayItems = [
-    {number: 1, farmer: 'Agricultor A', cpf: '111.222.333-44', date: '2021/09/07'},
-    {number: 2, farmer: 'Agricultor B', cpf: '111.222.333-44', date: '2021/09/07'},
-    {number: 3, farmer: 'Agricultor C', cpf: '111.222.333-44', date: '2021/09/07'},
-    {number: 4, farmer: 'Agricultor D', cpf: '111.222.333-44', date: '2021/09/07'},
-    {number: 5, farmer: 'Agricultor E', cpf: '111.222.333-44', date: '2021/09/07'},
-    {number: 6, farmer: 'Agricultor F', cpf: '111.222.333-44', date: '2021/09/07'}
+  const history = useHistory()
+
+  const [selectedGridRowsIds, setSelectedGridRowsIds] = useState<GridRowId[]>(
+    []
+  )
+  const [rows, setRows] = useState<GridRowsProp>([])
+
+  const columns: GridColumns = [
+    { field: 'id', headerName: 'Nome agricultor', type: 'string', flex: 1 },
+    { field: 'name', headerName: 'CPF', type: 'string', flex: 1 },
+    {
+      field: 'description',
+      headerName: 'Validade DAP',
+      type: 'string',
+      flex: 1
+    }
   ]
 
+  
+  useEffect(() => {
+    ;(async () => {
+      const response = await api.get('/farmer')
+      if (response.status === 200) {
+        const { data } = response
+        setRows(data)
+      }
+    })()
+  }, [])
+
+  const handleAddClick = () => {
+    history.push('/farmer/form')
+  }
+
+  const handleEditClick = () => {
+    if (!selectedGridRowsIds || selectedGridRowsIds.length < 1) {
+      return Swal.fire(
+        'Oops...',
+        'Você precisa selecionar 1 item para editá-lo',
+        'error'
+      )
+    } else if (selectedGridRowsIds.length > 1) {
+      return Swal.fire(
+        'Oops...',
+        'Somente é possivel editar 1 item por vez',
+        'error'
+      )
+    }
+
+    history.push(`/farmer/form?id=${selectedGridRowsIds[0]}`)
+  }
+
+  const handleDeleteClick = async () => {
+    if (!selectedGridRowsIds || selectedGridRowsIds.length < 1) {
+      return Swal.fire(
+        'Oops...',
+        'Você precisa selecionar pelo menos 1 item para remover',
+        'error'
+      )
+    }
+
+    const result = await Swal.fire({
+      title: 'Tem certeza que deseja remover esses itens?',
+      text: `Essa ação não pode ser revertida! Serão removidos ${selectedGridRowsIds.length} cestas de alimentos`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: `Sim, remover itens selecionados`,
+      cancelButtonText: 'Cancelar'
+    })
+
+    if (result.isConfirmed) {
+      selectedGridRowsIds.forEach(async id => {
+        const response = await api.delete(`/farmer/${id}`)
+        const { data } = response
+        setRows(rows.filter(e => e.id !== data.id))
+      })
+
+      Swal.fire(
+        'Itens removidos com sucesso!',
+        `Foram removidos ${selectedGridRowsIds.length} cestas de alimentos com sucesso.`,
+        'success'
+      )
+    }
+  }
+
+  const handleSelectionModelChange = (
+    selectionModel: GridSelectionModel,
+    _?: any
+  ) => {
+    const selectedIds = selectionModel.map(ids => ids)
+    setSelectedGridRowsIds(selectedIds)
+  }
+
   return (
-    <DefaultContainer breadcrumbs={['Agricultor']}>
-      <CustomTable
-        headerColumns={<>
-          <StyledTableCellCustom>Nome Aricultor</StyledTableCellCustom>
-          <StyledTableCellCustom>CPF</StyledTableCellCustom>
-          <StyledTableCellCustom>Validade DAP</StyledTableCellCustom>
-        </>}
-        itens={<>
-          {arrayItems.map(usuario => (
-            <StyledTableRow>
-              <StyledTableCellCustom>
-                <IconButton>
-                  <CreateIcon fontSize="small"></CreateIcon>
-                </IconButton>
-              </StyledTableCellCustom>
-              <StyledTableCellCustom>{usuario.farmer}</StyledTableCellCustom>
-              <StyledTableCellCustom>{usuario.cpf}</StyledTableCellCustom>
-              <StyledTableCellCustom>{usuario.date}</StyledTableCellCustom>
-              <StyledTableCellCustom>
-                <IconButton>
-                  <DeleteForeverIcon fontSize="small"></DeleteForeverIcon>
-                </IconButton>
-              </StyledTableCellCustom>
-            </StyledTableRow>
-          ))}
-        </>} itemCount={arrayItems.length} rowsPerPage={5} currentPage={1} handleChangePage={ ()=>{}
-        }
+    <DefaultContainer
+      breadcrumbs={['Agricultor']}
+      onAddClick={handleAddClick}
+      onEditClick={handleEditClick}
+      onDeleteClick={handleDeleteClick}
+    >
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        rowsPerPageOptions={[10, 20, 50, 100]}
+        paginationMode="server"
+        pagination={true}
+        onSelectionModelChange={handleSelectionModelChange}
+        autoHeight
+        checkboxSelection
       />
     </DefaultContainer>
   )
