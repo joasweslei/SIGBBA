17ac08b1038137023c4a742fdcfaa8ff8769b633
RobertsFerreira <robertsilva-ferreira@hotmail.com>
2021-09-29 22:23:11 -0300

    termino da base de autenticacao no front

--- packages/web/src/screens/auth/Login/index.tsx
+++ packages/web/src/screens/auth/Login/index.tsx
@@ -7,20 +7,24 @@ import {
   Toolbar,
   Typography
 } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import api from '../../../config/api'
 import { StyledLogin } from './styles'
 
 export const Login = () => {
   const history = useHistory()
 
+  const [username, setUsername] = useState('')
+  const [userpassword, setUserpassword] = useState('')
+
   const handleClick = async () => {
     // TODO: tratar erros na api e aqui
 
     const response = await api.post(`/users/auth/username`, {
-      username: 'robert',
-      userpassword: '1234'
+      username: username,
+      userpassword: userpassword
     })
 
     if (response.status === 200) {
@@ -28,7 +32,7 @@ export const Login = () => {
       if (data.access) {
         history.push('/user')
       } else {
-        console.log('Usuario ou senha inválidos')
+        Swal.fire('Login', 'Erro na verificação dos dados', 'warning')
       }
     }
   }
@@ -61,7 +65,13 @@ export const Login = () => {
             noValidate
             autoComplete="off"
           >
-            <TextField id="outlined-basic" label="User" variant="outlined" />
+            <TextField
+              id="outlined-basic"
+              label="User"
+              variant="outlined"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
           </Box>
           <Box
             component="form"
@@ -76,6 +86,8 @@ export const Login = () => {
               label="Senha"
               type="password"
               variant="outlined"
+              value={userpassword}
+              onChange={e => setUserpassword(e.target.value)}
             />
           </Box>
           <Button variant="contained" color="primary" onClick={handleClick}>
