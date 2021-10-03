0cb35b03740051a197f92fd7b60421c2ac4b1509
ThiagoMB60 <thiagomoreira60@gmail.com>
2021-10-03 15:41:42 -0300

    fix revertendo alterações na migration de criação da tabela order

---
+++ packages/server/src/app/controllers/OrderController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import Farmer from '../entities/Order'
+
+class OrderController {}
+
+export default new OrderController()
