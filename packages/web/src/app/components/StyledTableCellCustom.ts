290cd10fe0294d616bb4a8fa43db8f5fad5e1b8b
RachelBVecchi <88808799+RachelBVecchi@users.noreply.github.com>
2021-11-11 22:58:15 -0300

    desenvolvimento BD

--- packages/web/src/app/components/StyledTableCellCustom.ts
+++ packages/web/src/app/components/StyledTableCellCustom.ts
@@ -1,4 +1,4 @@
-import { styled } from '@material-ui/core'
+import styled from '@emotion/styled'
 import { StyledTableCell } from './StyledTableCell'
 
 export const StyledTableCellCustom = styled(StyledTableCell)(({ theme }) => ({
--- packages/web/src/app/components/StyledTableRow.ts
+++ packages/web/src/app/components/StyledTableRow.ts
@@ -1,14 +1,14 @@
-import { styled, TableRow } from '@material-ui/core'
+import styled from '@emotion/styled'
+import { TableRow } from '@mui/material'
 
 export const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  // hide last border
   '&:last-child td, &:last-child th': {
     border: 0
   },
   '&:hover': {
     border: 0,
     margin: 0,
-    backgroundColor: theme.palette.action.hover,
+
     cursor: 'pointer'
   }
 }))
